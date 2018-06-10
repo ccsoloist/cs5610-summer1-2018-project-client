c6aba6e5567780abc122b2b8613299d87cbd1239
ccsoloist <ccsoloist@gmail.com>
2018-06-10 02:04:02 -0400

    firsr commit

---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+export const SERVER = 'http://localhost:8080/api';
+// export const SERVER = 'https://cs5610-project-server.herokuapp.com/api';
+
+export const ALL_RESTAURANT_URL = SERVER + '/restaurant';
+export const REGISTER_URL = SERVER + '/register/TYPE';
+export const LOGIN_URL = SERVER + '/login/TYPE';
+export const LOGOUT_URL = SERVER + '/logout';
+export const PROFILE_URL = SERVER + '/profile';
+export const ALL_ORDERS_URL = SERVER + '/orders/TYPE/UID';
